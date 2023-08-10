65d0082beee10502c19db7e34ba7b53e2f077cda
JacekPardyak <jacek.pardyak@gmail.com>
2023-08-10 13:02:21 +0200

    ver 1

--- R/execute.R
+++ R/execute.R
@@ -45,7 +45,9 @@ def get_data(x):
   with Pyadomd(conn_str) as conn:
     with conn.cursor().execute(x) as cur:
       df = DataFrame(cur.fetchone(), columns=[i.name for i in cur.description])
-      df = df.fillna("NA") # needed to smoothly convert to R tibble
+      tmp = df.select_dtypes(include="object")
+      tmp.fillna("NULL", inplace = True)
+      df = tmp.join(df.select_dtypes(exclude="object"))
   return df' %>% sprintf(Sys.getenv("adomd_path"), Sys.getenv("conn_str")) %>% writeLines('lib.py')
     
     if ( nchar(theObject@mdx) == 0 & nchar(theObject@cube) == 0) {
