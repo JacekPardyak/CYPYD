45cb773ef8686d2b9269447f4574dd991a0fc55c
unknown <jacek.pardyak@gmail.com>
2023-08-10 20:57:51 +0200

    d

--- lib.py
+++ lib.py
@@ -1,11 +1,13 @@
 from sys import path
-path.append("")
+path.append("/Program Files/Microsoft.NET/ADOMD.NET/160")
 from pyadomd import Pyadomd
 from pandas import DataFrame
-conn_str = ""
+conn_str = "Data Source=localhost; Provider=MSOLAP; initial catalog=Analysis Services Tutorial"
 def get_data(x):
   with Pyadomd(conn_str) as conn:
     with conn.cursor().execute(x) as cur:
       df = DataFrame(cur.fetchone(), columns=[i.name for i in cur.description])
-      df = df.fillna("NA") # needed to smoothly convert to R tibble
+      tmp = df.select_dtypes(include="object")
+      tmp.fillna("NULL", inplace = True)
+      df = tmp.join(df.select_dtypes(exclude="object"))
   return df
--- state.py
+++ state.py
@@ -1,7 +1,7 @@
 from sys import path
-path.append("")
+path.append("/Program Files/Microsoft.NET/ADOMD.NET/160")
 from pyadomd import Pyadomd
-conn_str = ""
+conn_str = "Data Source=localhost; Provider=MSOLAP; initial catalog=Analysis Services Tutorial"
 def get_state():
   with Pyadomd(conn_str) as conn:
     df = print(conn.state)
