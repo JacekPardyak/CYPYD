a5d8bc80edd7698ecda75838d01e1b4d4a3015c5
unknown <jacek.pardyak@gmail.com>
2023-08-20 21:23:13 +0200

    clean

--- state.py
+++
@@ -1,8 +0,0 @@
-from sys import path
-path.append("/Program Files/Microsoft.NET/ADOMD.NET/160")
-from pyadomd import Pyadomd
-conn_str = "Data Source=localhost; Provider=MSOLAP; initial catalog=Analysis Services Tutorial"
-def get_state():
-  with Pyadomd(conn_str) as conn:
-    df = print(conn.state)
-    return df
